bd43227c3f9807c281d963ae9c941fc345e7836a
Sercantugba <sercantugba834@gmail.com>
2020-08-17 00:58:51 -0500

    deleted some files and functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import React from "react";
-import Home from "./pages/home.js"
-import Header from './header'
-import Main from './main'
+
 
 const App = () => (
   <div>
-    <Header />
-    <Main />
+   
   </div>
 )
 
