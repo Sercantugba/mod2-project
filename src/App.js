533e631cdc8bc649c3d03d34040eefd21b464535
Sercantugba <sercantugba834@gmail.com>
2020-08-17 00:41:29 -0500

    working on header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+import React from "react";
+import Home from "./pages/home.js"
+import Header from './header'
+import Main from './main'
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
+
 
 export default App;
