fbc398508e706d4de580d55f08168410e3af6a1c
Sercantugba <sercantugba834@gmail.com>
2020-08-18 17:09:35 -0500

    fetching data

--- src/pages/DashbordPage.js
+++ src/pages/DashbordPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SearchBar from '../components/SearchBar'
 
 
 const DashbordPage = () => (
   <section>
     <h1>Welcome to NBA Players website</h1>
     <p>Search for players</p>
+    <SearchBar />
     <Link to="/posts" className="button">
       View Players
     </Link>
