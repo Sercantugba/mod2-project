84c501ba3c179f81f96d22eec0fb76b1bdd62c07
Sercantugba <sercantugba834@gmail.com>
2020-08-18 18:58:30 -0500

    updated dashbordpage

--- src/pages/DashbordPage.js
+++ src/pages/DashbordPage.js
@@ -9,7 +9,7 @@ const DashbordPage = () => (
     <p>Search for players</p>
     <SearchBar />
     <Link to="/posts" className="button">
-      View Players
+      View All Players
     </Link>
   </section>
 )
