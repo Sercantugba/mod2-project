a0b8dd2df07fd7fd706e810114d417f556e6fbc4
Sercantugba <sercantugba834@gmail.com>
2020-08-19 10:08:00 -0500

    finalize post file

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 export const Posts = ({post})=>(
     <article>
-        <h2>{post.first_name} {post.last_name} {post.height} {post.team.city}</h2>
-        <p>{post.id}</p>
+        <h2>Player Name: {post.first_name} {post.last_name}, Player Height: {post.height_feet}, Team City Play For: {post.team.city}</h2>
+        <p>Player ID: {post.id}</p>
         
         {/* <p>{post.body.substring(0,100)}</p> */}
 
-        <Link to={`/posts/${post.id}`} className="post-button">
+        {/* <Link to={`/posts/${post.id}`} className="post-button">
         Back to Main page
-      </Link>
+      </Link> */}
  
     </article>
     
