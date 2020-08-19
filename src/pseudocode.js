286238a96305cc9b82388c1d54fa253092ebc99e
Sercantugba <sercantugba834@gmail.com>
2020-08-19 14:48:01 -0500

    final check

--- src/pseudocode.js
+++ src/pseudocode.js
@@ -1,6 +1,14 @@
 // Created react app called nba-players.
 
-// I have referenced the redux-demo-tutorial to render my components and fetch my data what we did in class.
+// For this project I have referenced redux-demo tutorial what we did in the class with shaniece.
+
+// To render my components and fetch my data used componentDidMount method.
+
+// I have 4 component folder and define action when I use fetch data I send message to repo.
+
+// Action is object type and payload when we called action creator it gives return object called action.
+
+// Post.js file is a single component that we generate our post.
 
 // Created components folder inside of the folder I have two  files Post.js and SearchBar.js.
 
@@ -10,6 +18,10 @@
 
 // Then I created pages folder inside of the pages folder I have two files dashbordpage.js and postpage.js files.
 
+// I have searchbar file which is a class component we first fetching data then used filter according to searchbar this filter function helpful to give us object.
+
 // I imported searchbar file from dashbordpage file and also have link for view all players.
 
 // Also created reducers folder created actions to get data.
+
+// Postreducer when we fetch data dispatch get post receive here.
