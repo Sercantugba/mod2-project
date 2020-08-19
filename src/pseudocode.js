76c1527cab87a86238432649efb87e0a247424f1
Sercantugba <sercantugba834@gmail.com>
2020-08-18 22:54:31 -0500

    cont working on pseudocode

--- src/pseudocode.js
+++ src/pseudocode.js
@@ -6,7 +6,7 @@
 
 // I also have actions folder to payloads of information thatn send data from our application to our store.
 
-// In the postActions.js file I used async and fetching data.
+// In the postActions.js file I used async and fetching data to Make use of async actions to send data to and receive data from a server.
 
 // Then I created pages folder inside of the pages folder I have two files dashbordpage.js and postpage.js files.
 
