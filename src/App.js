3bd785124960b2910308267976645f8b943abd02
Sercantugba <sercantugba834@gmail.com>
2020-08-18 23:29:09 -0500

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom'
+// import bdashbord and postpage to get data our components
 import DashbordPage from './pages/DashbordPage';
 
 import PostsPage from './pages/PostsPage';
