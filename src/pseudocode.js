3f567a99a92781b24faa8d9da2fe4521a283dfd1
Sercantugba <sercantugba834@gmail.com>
2020-08-18 22:52:32 -0500

    updated files

--- src/pseudocode.js
+++ src/pseudocode.js
@@ -1,5 +1,13 @@
-// Created react app called nba-players
-// I have referenced the redux-demo-tutorial to render my components and fetch my data what we did in class
-// Created components folder inside of the folder I have two  files Post.js and SearchBar.js
-// I also have actions folder to payloads of information thatn send data from our application to our store
-// In the postActions.js file I used async and fetching data 
+// Created react app called nba-players.
+
+// I have referenced the redux-demo-tutorial to render my components and fetch my data what we did in class.
+
+// Created components folder inside of the folder I have two  files Post.js and SearchBar.js.
+
+// I also have actions folder to payloads of information thatn send data from our application to our store.
+
+// In the postActions.js file I used async and fetching data.
+
+// Then I created pages folder inside of the pages folder I have two files dashbordpage.js and postpage.js files.
+
+// I imported searchbar file from dashbordpage file and also have link for view all players.
