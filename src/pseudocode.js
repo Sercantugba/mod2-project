c4095e7700419e022294eb2659ced7d9d7ab7112
Sercantugba <sercantugba834@gmail.com>
2020-08-18 22:38:45 -0500

    adding my pseudocode to my project

---
+++ src/pseudocode.js
@@ -0,0 +1,3 @@
+// Created react app called nba-players
+// I have referenced the redux-demo-tutorial to render my components and fetch my data what we did in class
+// Created components folder inside of the folder I have two  files Post.js and SearchBar.js
