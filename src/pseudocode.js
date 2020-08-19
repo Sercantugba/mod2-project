aca841c01676fee86e87a34d95262b44f5829116
Sercantugba <sercantugba834@gmail.com>
2020-08-18 22:44:09 -0500

    cont pseudocode.js

--- src/pseudocode.js
+++ src/pseudocode.js
@@ -2,3 +2,4 @@
 // I have referenced the redux-demo-tutorial to render my components and fetch my data what we did in class
 // Created components folder inside of the folder I have two  files Post.js and SearchBar.js
 // I also have actions folder to payloads of information thatn send data from our application to our store
+// In the postActions.js file I used async and fetching data 
